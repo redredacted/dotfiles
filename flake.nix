ae79574e28a5ac2cc270a2916a4849e6bbd59ee2
redredacted <redredacted@gmail.com>
2023-08-27 22:32:21 -0500

    setup nixos for poptop

--- flake.nix
+++ flake.nix
@@ -1,5 +1,20 @@
 {
   description = "Home Manager configuration of popcron";
+  
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://cache.nixos.org"
+    ];
+
+    extra-substituters = [
+      # Nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
@@ -22,8 +37,19 @@
         };
       };
       nixosConfigurations = {
-        poptop = pkgs.lib.nixosSystem {
-          modules = [ ./configuration.nix ]; 
+        "poptop" = nixpkgs.lib.nixosSystem {
+
+          modules = [ 
+	    ./configuration.nix 
+
+	    home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+	      home-manager.useUserPackages = true;
+
+	      home-manager.users.popcorn = import ./home.nix;
+	    }
+	  ]; 
+	  specialArgs = inputs;
         };
       };
     };
--- home.nix
+++ home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "user";
-  home.homeDirectory = "/home/user";
+  home.username = "popcorn";
+  home.homeDirectory = "/home/popcorn";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -17,15 +17,20 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    pkgs.zig
-    pkgs.cargo
-    pkgs.nodejs_20
-    pkgs.cmake
-    pkgs.gnumake
-    pkgs.gcc
-    pkgs.ripgrep
-    pkgs.python312
+  home.packages = with pkgs; [
+    zig
+    cargo
+    nodejs_20
+    cmake
+    gnumake
+    gcc
+    ripgrep
+    python312
+    neofetch
+    jq
+    yq-go
+    tree
+    btop
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
@@ -34,7 +39,7 @@
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
