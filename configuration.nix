93bd7db3894462de3a51f4f89e881b3057a3dc96
redredacted <redredacted@gmail.com>
2023-08-27 23:01:35 -0500

    setup experimental features and updated flake.lock

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
     ];
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
